735bce2e808d97e0aa6c7d5d1835fa8b51e36eed
Aaron-R-Carter <aaronrobertcarter@gmail.com>
2019-10-10 21:15:40 -0400

    reworked the layout, added event listener instead of onlick, added a clear button, added a length input form

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,36 +1,62 @@
 // generate random password
 
-function generate() {
+var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*_+";
+var password = "";
+var generateBtn = document.querySelector("#generate-btn");
+var clipboardBtn = document.querySelector("#clipboard-btn");
+var clearBtn = document.querySelector("#clear-btn");
+
+// on button generate click
+
+generateBtn.addEventListener("click", function () {
 
     //set password length
 
-    var pwLength = 8;
+    var pwLength = document.getElementById("length-selector").value;
+
 
     //password values
-    var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*_+";
 
-    var password = "";
+    if (pwLength > 7 && pwLength < 129) {
 
-    //create for loop to choose password characters
+        //choose password characters
 
-    for (var i = 1; i <= pwLength; i++) {
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)))
+        for (var i = 1; i <= pwLength; i++) {
+            password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)))
+        }
+
+        // add password to textbox/display area
+
+        document.getElementById("display").value = password;
+
     }
 
-    // add password to textbox/display area
-
-    document.getElementById("display").value = password;
-}
-
-// set default length display 
-
-document.getElementById("length").innerHTML = "Length: 8"
+    else {
+        alert("Your password must be between 8 and 128 characters, please try again");
+    }
 
 
-function copyPassword(){
-    document.getElementById("display").select(); 
+});
+
+
+// on clipboard button click
+
+clipboardBtn.addEventListener("click", function() {
+
+    document.getElementById("display").select();
     document.execCommand("Copy");
     alert("Password Copied to Clipboard");
-}
+
+});
+
+// on clear button click
+
+clearBtn.addEventListener("click", function(){
+
+    document.getElementById("my-form").reset();
+
+})
+
+
 
 
